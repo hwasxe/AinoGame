a73e9f4a119a00a780c57410a86277546da38a6a
mustafaalann <mustafa.alann@hotmail.com>
2022-12-20 20:24:22 +0300

    RandomWalkMonster

--- Assets/Scripts/MonsterChase.cs
+++ Assets/Scripts/MonsterChase.cs
@@ -7,17 +7,39 @@ public class MonsterChase : MonoBehaviour
 {
     public GameObject Player;
     private NavMeshAgent NavMonster;
-    
+    float CurrentTime;
+    private Animator animator;
+
+    private Vector3 randomVector;
     // Start is called before the first frame update
     void Start()
     {
+        
+        animator = GetComponent<Animator>();
         NavMonster = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        NavMonster.SetDestination(Player.transform.position);
+        if (Vector3.Distance(Player.transform.position, transform.position) < 10)
+        {
+            animator.SetTrigger("move");
+            NavMonster.SetDestination(Player.transform.position);
+        }
+        else
+        {
+            
+            CurrentTime += Time.deltaTime;
+            if(CurrentTime > 4){
+                animator.SetTrigger("walk");
+                randomVector = new Vector3(UnityEngine.Random.Range(-10, 10), 0, UnityEngine.Random.Range(-10, 10));
+                NavMonster.SetDestination(randomVector);
+                CurrentTime = 0;
+            }
+            
+            
+        }
 
     }
 }
