e42a3e23cae1e8ed7efa32921d09767a43a3f498
mustafaalann <mustafa.alann@hotmail.com>
2022-12-20 18:42:54 +0300

    Move Face

---
+++ Assets/Scripts/sfaceScript.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sfaceScript : MonoBehaviour
+{
+
+    public GameObject sface;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sface.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            sface.SetActive(true);
+            StartCoroutine(TimerStart());
+        }
+    }
+
+    IEnumerator TimerStart()
+    {
+        yield return new WaitForSeconds(1);
+        sface.SetActive(false);
+        Destroy((gameObject));
+    }
+}
